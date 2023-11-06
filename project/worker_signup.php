aeb664ede5d81f63593b4055043acdc38c15b12e
fatimaamuh <125767379+fatimaamuh@users.noreply.github.com>
2023-11-06 20:27:23 +0300

    Update worker_signup.php

--- project/worker_signup.php
+++ project/worker_signup.php
@@ -1,34 +1,23 @@
+
 <?php
 include 'config.php';
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Establish a connection to the database
-    $servername = "your_servername";  // Your MySQL server name
-    $username = "your_username";     // Your MySQL username
-    $password = "your_password";     // Your MySQL password
-    $dbname = "b_lkhidma_db";        // Your database name
-
-    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Prepare and bind the INSERT SQL statement
     $stmt = $conn->prepare("INSERT INTO Service_Provider (Fname, Email, Phone, Area, Pass) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $fname, $email, $phone, $area, $password);
 
-    // Set parameters and execute
     $fname = $_POST['username'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $area = $_POST['area'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Attempt to execute the prepared statement
     if ($stmt->execute()) {
         echo "New record created successfully";
     } else {
